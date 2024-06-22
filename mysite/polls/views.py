0f4b0b437a34eba9a9a52cdbefc99a46ee813dd0
Nagesh <npknagesh@gmail.com>
2024-06-23 01:30:44 +0530

    add templates to render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Question
+# from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Hello world, You are at polls index')
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
+
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk = question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/index.html", {'question': question})
+
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at results of the question {question_id}")
+
+
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}")
 
