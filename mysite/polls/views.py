6042bda6587ad44fecfd3f5f99def036f902c082
Nagesh <npknagesh@gmail.com>
2024-06-23 15:08:47 +0530

    edit the form to accept POST req:

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,7 @@
+from django.db.models import F
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 from .models import Choice, Question
 # from django.template import loader
 
@@ -20,12 +22,14 @@ def detail(request, question_id):
     #     raise Http404("Question does not exist")
     # return HttpResponse(f"You're looking at question {question_id}")
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/index.html", {'question': question})
+    return render(request, "polls/detail.html", {'question': question})
 
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at results of the question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {'question': question})
+    # return HttpResponse(f"You're looking at results of the question {question_id}")
 
 
 
@@ -45,5 +49,7 @@ def vote(request, question_id):
         )
     else:
         selected_choice.votes = F("votes") + 1
-    return HttpResponse(f"You are voting on question {question_id}")
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    # return HttpResponse(f"You are voting on question {question_id}")
 
