315f6c57d252449549a9521e5e29ef6ef40f8425
Nagesh <npknagesh@gmail.com>
2024-06-23 14:53:58 +0530

    modify templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Question
+from .models import Choice, Question
 # from django.template import loader
 
 
@@ -31,5 +31,19 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question":question,
+                "error_message": "You did not select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
     return HttpResponse(f"You are voting on question {question_id}")
 
